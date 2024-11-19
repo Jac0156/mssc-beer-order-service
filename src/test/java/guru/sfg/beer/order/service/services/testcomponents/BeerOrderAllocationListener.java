82f9f46b6b8947a63342dee406ad945993f4875e
Jack Weby <jac0156@hotmail.com>
2024-11-19 11:49:58 -0500

    Finish allocation test #29

--- src/test/java/guru/sfg/beer/order/service/services/testcomponents/BeerOrderAllocationListener.java
+++ src/test/java/guru/sfg/beer/order/service/services/testcomponents/BeerOrderAllocationListener.java
@@ -17,13 +17,15 @@ import lombok.extern.slf4j.Slf4j;
 public class BeerOrderAllocationListener {
 
     private final JmsTemplate jmsTemplate;
+
     @JmsListener(destination = JmsConfig.ALLOCATE_ORDER_QUEUE)
-    
     public void listen(Message msg){
         AllocateOrderRequest request = (AllocateOrderRequest) msg.getPayload();
+        
         request.getBeerOrderDto().getBeerOrderLines().forEach(beerOrderLineDto -> {
             beerOrderLineDto.setQuantityAllocated(beerOrderLineDto.getOrderQuantity());
         });
+
         jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_RESPONSE_QUEUE,
                 AllocateOrderResult.builder()
                 .beerOrderDto(request.getBeerOrderDto())
